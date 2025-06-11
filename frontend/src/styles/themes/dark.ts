b18c35d187618109baf0c1b502f8faa8f6b618dc
valeria-knv <115754671+valeria-knv@users.noreply.github.com>
2025-06-11 15:08:52 +0300

    Add files via upload

---
+++ frontend/src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'dark',
+  
+  colors: {
+    primary: '#333', 
+    primaryHover: '#444', 
+    secundary: '#A50104',
+    secundaryHover: '#B91414',
+
+    background: '#222',
+    text: '#fff',
+
+    accent: '#6f42c1',
+    accentDark: '#5936a2',
+  }  
+};
---
+++ frontend/src/styles/themes/light.ts
@@ -0,0 +1,15 @@
+export default {
+  title: 'light',
+  
+  colors: {
+    primary: '#A50104', 
+    primaryHover: '#B91414',
+    secundary: '#7C7C7C',
+    secundaryHover: '#9E9E9E',
+
+    background: '#f5f5f5',
+    text: '#fff',
+    accent: '#6f42c1',
+    accentDark: '#5936a2',
+  }  
+};
